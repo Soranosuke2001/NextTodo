0d4871427a250ad699a532d26fed8c07ec9958fb
Sora <sorazora1.4@gmail.com>
2023-05-23 13:20:03 -0700

    basic todos list page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,32 @@
-export default function Home() {
+import { prisma } from "@/db";
+import Link from "next/link";
+
+export default async function Home() {
+  const todos = await prisma.todo.findMany();
+
+  // await prisma.todo.create({
+  //   data: {
+  //     title: "test",
+  //     complete: false,
+  //   },
+  // });
+
   return (
-    <main className="flex justify-center items-center text-center">
-      <h1>Home Page</h1>
-    </main>
+    <>
+      <header className="flex justify-between items-center mb-4">
+        <h1 className="text-2xl">Todos</h1>
+        <Link
+          href="/new"
+          className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+        >
+          New
+        </Link>
+      </header>
+      <ul className="pl-4">
+        {todos.map((todo) => {
+          return <li key={todo.id}>{todo.title}</li>;
+        })}
+      </ul>
+    </>
   );
 }
